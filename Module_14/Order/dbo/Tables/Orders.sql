de3045d2b6a585b72e7adbd9315d671eaffd94aa
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2023-01-17 20:38:38 +0100

    Import DB

---
+++ Module_14/Order/dbo/Tables/Orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [order_id]     INT      IDENTITY (1, 1) NOT NULL,
+    [status]       INT      NOT NULL,
+    [created_date] DATETIME NOT NULL,
+    [updated_date] DATETIME NOT NULL,
+    [product_id]   INT      NOT NULL,
+    CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([order_id] ASC),
+    CONSTRAINT [FK_order_product] FOREIGN KEY ([product_id]) REFERENCES [dbo].[Products] ([product_id])
+);
+
